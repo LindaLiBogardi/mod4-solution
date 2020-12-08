c40ae598626a6aaf50155065724d6037d7aaa0c4
Linda Li Bogardi <linli781@gmail.com>
2020-12-08 22:48:50 +0100

    moved helloSpeaker in the wrap

--- SpeakHello.js
+++ SpeakHello.js
@@ -8,23 +8,19 @@
 // var helloSpeaker =
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
-var helloSpeaker() {
-   function b(){
-       console.log("Hello" + "" + name);
-   } 
-}
-b();
+
 
 (function() {
-    var speakWord = "Hello";
-
+      var helloSpeaker = {};
+      helloSpeaker.speakWord = "Hello";
+  
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-   helloSpeaker.speak = function () {
-      console.log(speakWord + " " + name);
-}
-window.helloSpeaker = helloSpeaker ();
+   helloSpeaker.speak = function (name) {
+      console.log(helloSpeaker.speakWord + " " + name);
+    }
+window.helloSpeaker = helloSpeaker;
 
 })(window);
 
